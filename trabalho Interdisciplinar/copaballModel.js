4f5c423a3b24b8f64fc1d9bc63e0624b2982ca03
Juliasacht <juliasacht01@gmail.com>
2025-03-10 14:27:51 -0300

    fazendofiltros

--- trabalho Interdisciplinar/copaballModel.js
+++ trabalho Interdisciplinar/copaballModel.js
@@ -1,29 +1,99 @@
-export function filtrarColaboradoresPorTime(colaboradores, time) {
-    return colaboradores.filter(colaborador => colaborador.time === time);
-}
+import * as Dados from "./copaballDados.js";
+import { Jogador } from "./classes/jogador.js";
+import { Tecnico } from "./classes/tecnico.js";
+import { Diretor } from "./classes/diretor.js";
 
-export function filtrarColaboradoresPorNome(colaboradores, nome) {
-    return colaboradores.filter(colaborador => colaborador.nome.toLowerCase().includes(nome.toLowerCase()));
-}
-
-export function filtrarJogadores(colaboradores) {
-    return colaboradores.filter(colaborador => colaborador.cargo === "Jogador");
-}
-
-export function filtrarTecnicos(colaboradores) {
-    const tecnicosUnicos = new Map();
-    colaboradores.forEach(colaborador => {
-        if (colaborador.cargo === "Técnico" && !tecnicosUnicos.has(colaborador.time)) {
-            tecnicosUnicos.set(colaborador.time, colaborador);
+function filtrarTimesNome(nome) {
+    for (i = 0; i < Dados.vetTimes.lenght; i++) {
+        if (Dados.vetTimes[i].nomeTime == nome) {
+            return Dados.vetTimes[i];
+        } else {
+            return null;
         }
-    });
-    return Array.from(tecnicosUnicos.values());
+    }
 }
 
-export function filtrarPartidasPorData(partidas, data) {
-    return partidas.filter(partida => partida.data === data);
+function mostrarColabTimes(time) {
+    for (let i = 0; i < Dados.vetTimes.lenght; i++) {
+        if (Dados.vetTimes[i].nomeTime.toUpperCase() == time.toUpperCase()) {
+            return Dados.vetTimes[i].colaboradores;
+        } else {
+            return null;
+        }
+    }
 }
 
-export function filtrarPartidasPorTime(partidas, time) {
-    return partidas.filter(partida => partida.time1 === time || partida.time2 === time);
+function nomeColab(nome) {
+    var vetFiltrado = [];
+    for (let i = 0; i < Dados.vetColaboradores.lenght; i++) {
+        if (Dados.vetColaboradores[i].nome.includes(nome)) {
+            vetFiltrado.push(Dados.vetColaboradores[i]);
+        }
+    }
 }
+
+function filtrarCargos(cargo) {
+    var vetFiltrado = [];
+
+    switch (cargo) {
+        case "Jogador":
+            for (let i = 0; i < Dados.vetColaboradores.lenght; i++) {
+                if (Dados.vetColaboradores[i] instanceof Jogador) {
+                    vetFiltrado.push(Dados.vetColaboradores[i]);
+                }
+            }
+            break;
+        case "Tecnico":
+            for (let i = 0; i < Dados.vetColaboradores.lenght; i++) {
+                if (Dados.vetColaboradores[i] instanceof Tecnico) {
+                    vetFiltrado.push(Dados.vetColaboradores[i]);
+                }
+            }
+            break;
+        case "Diretor":
+            for (let i = 0; i < Dados.vetColaboradores.lenght; i++) {
+                if (Dados.vetColaboradores[i] instanceof Diretor) {
+                    vetFiltrado.push(Dados.vetColaboradores[i]);
+                }
+            }
+            break;
+    }
+
+    return vetFiltrado;
+}
+
+function filtrarTecnicosTime (time) {
+    for (let i = 0; i < Dados.vetTimes.lenght; i++) {
+        if (Dados.vetTimes[i].nome == time) {
+            for(let j = 0; j < Dados.vetTimes[i].colaboradores.lenght; j++) {
+                if (Dados.vetTimes[i].colaboradores[j] instanceof Tecnico) {
+                    return Dados.vetTimes[i].colaboradores[j];
+                }
+            }
+        } else {
+            return null;
+        }
+    }
+}
+
+function filtrarPartidasData(data) {  
+    for (let i = 0; i < Dados.vetPartidas.lenght; i++) {
+        if (Dados.vetPartidas[i].dataPartida.includes(data)) {
+            return Dados.vetPartidas[i];
+        } else {
+            return null;
+        }
+    }
+}
+
+function filtrarPartidasTime(time) {
+    for (let i = 0; i < Dados.vetPartidas.lenght; i++) {
+        if (Dados.vetPartidas[i].timeA == time || Dados.vetPartidas[i].timeB == time) {
+            return Dados.vetPartidas[i];
+        } else {
+            return null;
+        }
+    }
+}
+
+export {filtrarTimesNome, mostrarColabTimes, nomeColab, filtrarCargos, filtrarTecnicosTime, filtrarPartidasData, filtrarPartidasTime};
--- trabalho Interdisciplinar/copaballView.js
+++ trabalho Interdisciplinar/copaballView.js
@@ -1,21 +1,24 @@
-import * as Dados from "./copaballDados.js";
+import * as Model from "./copaballModel.js";
 
 const timeEscolhido = "Flamengo";
 
-console.log("|| Colaboradores do Time ||");
-console.log(filtrarColaboradoresPorTime(Dados.colaboradores, timeEscolhido));
+console.log("|| Colaboradores do Time Flamengo ||");
+console.log(Model.mostrarColabTimes(timeEscolhido));
 
+const nome = "Luiz";
 console.log("|| Colaboradores que tenham 'silva' no nome ||");
-console.log(filtrarColaboradoresPorNome(Dados.colaboradores, "silva"));
+console.log(Model.nomeColab(nome));
 
 console.log("|| Mostrando os jogadores da temporada ||");
-console.log(filtrarJogadores(Dados.colaboradores));
+console.log(Model.filtrarCargos(cargo));
 
 console.log("|| Mostrando técnicos dos times ||");
-console.log(filtrarTecnicos(Dados.colaboradores));
+console.log(Model.filtrarTecnicosTime (time));
 
+const data = "10/02";
 console.log("|| Mostrando as partidas que aconteceram no dia 10 de abril ||");
-console.log(filtrarPartidasPorData(Dados.partidas, "10 de abril"));
+console.log(Model.filtrarPartidasData(data));
 
+const time = "Flamengo";
 console.log("|| Mostrando as partidas do time ||");
-console.log(filtrarPartidasPorTime(Dados.partidas, timeEscolhido));
+console.log(Model.filtrarPartidasTime(time));
